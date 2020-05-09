38e6ac3816652b567e99ece1ad43f361185d2dca
Shuto41 <shuto13ks@gmail.com>
2020-05-09 12:28:40 +0900

    これから共通パーツを作っていきます。

---
+++ cssjs/asoboze_2020.js
@@ -0,0 +1,51 @@
+// JavaScript Document
+$(function() {
+
+    $("#pcthreemenu2").load("https://www.rakuten.ne.jp/gold/asoboze/index2020_beta.html .topthree2");
+    $("#pcthreemenu3").load("https://www.rakuten.ne.jp/gold/asoboze/index2020_beta.html .topthree3");
+	$("#othergallery").load("https://www.rakuten.ne.jp/gold/asoboze/gallery/parts/othergallery.html .swiper-container");
+    $("#spheader").load("https://www.rakuten.ne.jp/gold/asoboze/parts/header_aso.html");
+    $("#footer_aso").load("https://www.rakuten.ne.jp/gold/asoboze/parts/footer_aso.html");
+});
+$(function(){
+  $('.effect .hop').css({"opacity":"0", "padding-top":"10px"});
+  $(window).scroll(function (){
+    $(".effect").each(function(){
+      var imgPos = $(this).offset().top;    
+      var scroll = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      if (scroll > imgPos - windowHeight + windowHeight/10){
+        $(".hop",this).css({"opacity":"1", "padding-top": "0" });
+        $("i",this).css({ 
+          "font-size": "100px",
+          "padding": "0 20px 40px"
+        });
+      } 
+    });
+  });
+});
+        $(function() {
+        $(".icon-wrap").click(function() {
+            $(this).toggleClass("active");
+            $("body").toggleClass("fixing");
+            $(".iframe_header").toggleClass("iframe_touched");
+        });
+    });
+$(function() {
+    $(".iframe_open").click(function() {
+        $(".iframe_header").toggleClass("iframe_touched");
+    })
+})
+var menuHeight = 50;
+var startPos = 0;
+$(window).scroll(function(){
+  var currentPos = $(this).scrollTop();
+  if (currentPos > startPos && $("body").hasClass("fixing") == false) {
+    if($(window).scrollTop() >= 200) {
+      $(".spheader_aso").css("top", "-" + 50 + "px");
+    }
+  } else {
+    $(".spheader_aso").css("top", 0 + "px");
+  }
+  startPos = currentPos;
+});  
